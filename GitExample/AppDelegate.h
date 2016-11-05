6034b5f646fd878677d4ba7faf5ded2ff2a9babd
anil gupta <anil03gupta03@gmail.com>
2016-11-05 13:29:57 +0530

    Initial Commit

---
+++ GitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitExample
+//
+//  Created by Anil Gupta on 05/11/16.
+//  Copyright © 2016 Anil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitExample
+//
+//  Created by Anil Gupta on 05/11/16.
+//  Copyright © 2016 Anil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
